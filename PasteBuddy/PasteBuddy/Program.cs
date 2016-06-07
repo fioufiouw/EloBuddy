8eeb0c752efb0f20a38c3dd80463151c9495ec10
Adam <adamnie3334@gmail.com>
2016-06-08 00:23:32 +0100

    ADD PASTEBUDDY

---
+++ PasteBuddy/PasteBuddy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using System.Windows.Forms;
+
+namespace PasteBuddy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Chat.OnInput += Chat_OnInput;
+        }
+
+        private static void Chat_OnInput(ChatInputEventArgs args)
+        {
+            if (args.Input.Trim() == "/paste")
+            {
+                if (string.IsNullOrEmpty(Clipboard.GetText()))
+                    Chat.Print("Nice try Kappa - Clipboard is empty FeelsBadMan");
+                else
+                    args.Input = Clipboard.GetText();
+            }
+
+            if (args.Input.Contains("_paste_"))
+            {
+                if (string.IsNullOrEmpty(Clipboard.GetText()))
+                    Chat.Print("Nice try Kappa - Clipboard is empty FeelsBadMan");
+                else
+                    args.Input = args.Input.Replace("_paste_", Clipboard.GetText());
+            }
+        }
+    }
+}
